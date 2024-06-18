07d8fc25ea64ccfd01f9ade53e1bc0d29810037d
Jesús Enrique Rascón <blograso@gmail.com>
2024-06-18 13:26:52 +0200

    scroll behavior

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
   import { formatDate } from "$lib/utils";
-  import Toc from "svelte-toc";
-  import type { Post } from "$lib/types";
+  import Toc from "$lib/components/Toc.svelte";
 
-  import { page } from "$app/stores";
   import { onMount } from "svelte";
 
   export let data;
 
   const { title, date, categories, description } = data.metadata;
 
-  let url = new URL("https://jesirgb.com/blog/thumbnails");
-  url.searchParams.append("title", title);
-
-  url.searchParams.append("desc", description);
+  // let url = new URL("https://jesirgb.com/blog/thumbnails");
+  // url.searchParams.append("title", title);
+  // url.searchParams.append("desc", description);
 
   let asideToc: HTMLElement;
   let markdownContent: HTMLElement;
 
-  let mounted: false;
+  let mounted: boolean = false;
   onMount(() => {
     mounted = true;
   });
 </script>
 
-<article class="mx-auto my-24 @container max-w-[93%] md:max-w-xl xl:max-w-6xl">
+<article class="mx-auto my-24 @container max-w-[93%] md:max-w-2xl xl:max-w-6xl">
   <hrgoup
     class="flex flex-col space-y-3 border-b border-primary-content my-20 pb-5"
   >
@@ -45,16 +42,16 @@
     </main>
     <div class="w-0 xl:w-1/4">
       {#if mounted}
-        <Toc pageBody={markdownContent} autoHide={false} aside={asideToc}>
+        <Toc
+          --aside-toc-mobile="red"
+          breakpoint={1300}
+          pageBody={markdownContent}
+          autoHide={false}
+          aside={asideToc}
+        >
           <span slot="title" class="font-bold text-lg">Map</span>
         </Toc>
       {/if}
     </div>
   </div>
 </article>
-
-<style>
-  aside {
-    color: red;
-  }
-</style>
