60fa1c42156256604282080625c02327a353c747
Jesús Enrique Rascón <blograso@gmail.com>
2024-06-22 01:37:16 +0200

    install threlte

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { T } from '@threlte/core'
+</script>
+
+<T.PerspectiveCamera
+    makeDefault
+    position={[10, 10, 10]}
+    on:create={({ ref }) => {
+        ref.lookAt(0, 0, 0)
+    }}
+/>
+
+<T.Mesh>
+    <T.BoxGeometry args={[1, 1, 1]} />
+    <T.MeshBasicMaterial color="red" />
+</T.Mesh>
---
+++ src/routes/trelte/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { Canvas } from '@threlte/core'
+    // import Duck from '../../../static/yellow_rubber_duck.svelte'
+</script>
+
+<div>hola</div>
