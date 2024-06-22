3f6fa8a094e21cf3ca9dce671b7c9550a4c0fca3
Jesús Enrique Rascón <blograso@gmail.com>
2024-06-23 01:03:23 +0200

    working duck

--- src/routes/trelte/+page.svelte
+++ src/routes/trelte/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-    import { Canvas } from '@threlte/core'
-    // import Duck from '../../../static/yellow_rubber_duck.svelte'
+    import { Canvas } from "@threlte/core";
+    import { T } from "@threlte/core";
+
+    import { interactivity, OrbitControls } from "@threlte/extras";
+    import Duck from "$lib/yellow_rubber_duck.svelte";
 </script>
 
-<div>hola</div>
+<div class="border w-full h-[800px] mx-auto my-20">
+    <Canvas>
+        <T.PerspectiveCamera
+            makeDefault
+            position={[10, 10, 10]}
+            on:create={({ ref }) => {
+                ref.lookAt(0, 1, 0);
+            }}
+        >
+            <OrbitControls />
+        </T.PerspectiveCamera>
+        <Duck />
+    </Canvas>
+</div>
