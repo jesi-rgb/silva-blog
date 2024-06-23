bd3faed0bf1615572a3013d3b7067a6e0404e8dc
Jesús Enrique Rascón <blograso@gmail.com>
2024-06-23 13:22:47 +0200

    moving duck

--- src/routes/trelte/+page.svelte
+++ src/routes/trelte/+page.svelte
@@ -1,22 +1,162 @@
 <script lang="ts">
     import { Canvas } from "@threlte/core";
     import { T } from "@threlte/core";
+    import Scroller from "@sveltejs/svelte-scroller";
 
-    import { interactivity, OrbitControls } from "@threlte/extras";
+    import { OrbitControls } from "@threlte/extras";
     import Duck from "$lib/yellow_rubber_duck.svelte";
+
+    let progress: number;
+
+    $: rotation = progress * Math.PI;
+    $: duckPosition = [0, 2 * progress, 0];
+    $: console.log(progress);
 </script>
 
-<div class="border w-full h-[800px] mx-auto my-20">
-    <Canvas>
-        <T.PerspectiveCamera
-            makeDefault
-            position={[10, 10, 10]}
-            on:create={({ ref }) => {
-                ref.lookAt(0, 1, 0);
-            }}
-        >
-            <OrbitControls />
-        </T.PerspectiveCamera>
-        <Duck />
-    </Canvas>
-</div>
+<main class="my-20 w-3/4 mx-auto">
+    <div class="mb-32 text-xl flex flex-col gap-4">
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+    </div>
+    <Scroller bind:progress>
+        <div slot="background" class="h-[100vh] relative">
+            <div class="w-full mx-auto h-full border-2 absolute">
+                <Canvas>
+                    <T.PerspectiveCamera
+                        makeDefault
+                        position={[10, 10, 10]}
+                        on:create={({ ref }) => {
+                            ref.lookAt(...duckPosition);
+                        }}
+                    >
+                        <OrbitControls />
+                    </T.PerspectiveCamera>
+                    <T.Group
+                        rotation.y={rotation}
+                        position={duckPosition}
+                        scale={[2, 2, 2]}
+                    >
+                        <Duck />
+                    </T.Group>
+                </Canvas>
+            </div>
+        </div>
+
+        <div slot="foreground" class="w-3/4 mx-auto">
+            <section class="pt-32">
+                <div class="font-bold text-3xl">pato</div>
+                <div>
+                    asdfklasñldf jaslñkdjf añlsdkjf sldkfj saldfñajsl dfkjs
+                    ldfkjsldkf jsldfkj
+                </div>
+            </section>
+
+            <section class="pt-32">
+                <div class="font-bold text-3xl">pato</div>
+                <div>
+                    asdfklasñldf jaslñkdjf añlsdkjf sldkfj saldfñajsl dfkjs
+                    ldfkjsldkf jsldfkj
+                </div>
+            </section>
+            <section class="pt-32">
+                <div class="font-bold text-3xl">pato</div>
+                <div>
+                    asdfklasñldf jaslñkdjf añlsdkjf sldkfj saldfñajsl dfkjs
+                    ldfkjsldkf jsldfkj
+                </div>
+            </section>
+            <section class="pt-32">
+                <div class="font-bold text-3xl">pato</div>
+                <div>
+                    asdfklasñldf jaslñkdjf añlsdkjf sldkfj saldfñajsl dfkjs
+                    ldfkjsldkf jsldfkj
+                </div>
+            </section>
+        </div>
+    </Scroller>
+
+    <div class="mt-32 text-xl flex flex-col gap-4">
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+
+        <p>
+            Officia sit nulla exercitation tempor dolore proident reprehenderit
+            proident. Veniam sint veniam incididunt minim excepteur ut velit
+            quis eiusmod culpa consequat quis est elit. Ut est qui elit amet
+            eiusmod culpa esse. Dolor commodo culpa do veniam cillum amet in
+            anim. Tempor non nostrud excepteur cupidatat nisi. Culpa occaecat
+            exercitation eiusmod. Id sunt deserunt fugiat irure dolore fugiat
+            elit ullamco Lorem est exercitation dolore ad ex.
+        </p>
+    </div>
+</main>
+
+<style>
+    section {
+        height: 80vh;
+    }
+</style>
