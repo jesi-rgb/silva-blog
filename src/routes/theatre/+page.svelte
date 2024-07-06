914634e1ca81eced5601979c642ccc1762ccafd8
Jesús Enrique Rascón <blograso@gmail.com>
2024-07-06 19:20:57 +0200

    almost working custom scroller

--- src/routes/theatre/+page.svelte
+++ src/routes/theatre/+page.svelte
@@ -1,28 +1,24 @@
 <script lang="ts">
-    import { T } from '@threlte/core'
-    import { OrbitControls } from '@threlte/extras'
-    import { SheetObject } from '@threlte/theatre'
+    import { T } from "@threlte/core";
+    import { OrbitControls } from "@threlte/extras";
+    import { SheetObject } from "@threlte/theatre";
 
-    import { Canvas } from '@threlte/core'
-    import { Theatre } from '@threlte/theatre'
+    import { Canvas } from "@threlte/core";
+    import { Theatre } from "@threlte/theatre";
 </script>
 
 <div class="w-full h-[100vh] border">
     <Canvas>
         <Theatre>
-            <SheetObject key="Camera" let:Transform>
-                <Transform>
-                    <T.PerspectiveCamera makeDefault position={[5, 10, 3]} />
-                </Transform>
+            <SheetObject key="Camera">
+                <T.PerspectiveCamera makeDefault position={[5, 10, 3]} />
             </SheetObject>
 
-            <SheetObject key="Cube" let:Transform>
-                <Transform>
-                    <T.Mesh position.y={0.5}>
-                        <T.BoxGeometry />
-                        <T.MeshBasicMaterial color="hotpink" />
-                    </T.Mesh>
-                </Transform>
+            <SheetObject key="Cube">
+                <T.Mesh position.y={0.5}>
+                    <T.BoxGeometry />
+                    <T.MeshBasicMaterial color="hotpink" />
+                </T.Mesh>
             </SheetObject>
 
             <T.GridHelper />
--- src/routes/trelte/+page.svelte
+++ src/routes/trelte/+page.svelte
@@ -1,29 +1,66 @@
 <script lang="ts">
-    import { Canvas } from '@threlte/core'
-    import { T } from '@threlte/core'
-    import Scroller from '@sveltejs/svelte-scroller'
-    import Container from '$lib/components/Container.svelte'
+    import { Canvas } from "@threlte/core";
+    import { onMount } from "svelte";
+    import { T } from "@threlte/core";
+    import Scroller from "@sveltejs/svelte-scroller";
+    import Container from "$lib/components/Container.svelte";
+    import { spring } from "svelte/motion";
 
-    import { OrbitControls } from '@threlte/extras'
-    import Duck from '$lib/yellow_rubber_duck.svelte'
+    import { OrbitControls } from "@threlte/extras";
+    import Duck from "$lib/yellow_rubber_duck.svelte";
 
-    let progress: number
+    let progress: number;
+    let count: number;
+    let index: number;
+    let offset: number;
 
-    let top = 0.0
-    let threshold = 0.2
-    let bottom = 0.9
+    let top = 0.0;
+    let threshold = 0.2;
+    let bottom = 0.9;
 
-    $: duckPosition = [progress * 10, progress * 5, 10]
-    $: rotation = progress * Math.PI
+    function interpolate(a, b, t) {
+        return {
+            x: a.x + (b.x - a.x) * t,
+            y: a.y + (b.y - a.y) * t,
+            z: a.z + (b.z - a.z) * t,
+        };
+    }
 
-    // TODO: build an object that serves as "keyframes"
-    // for indicating where the model should go at what point
-    // maybe a mapping object -> section and interpolation
-    // methods from claude
+    const position = spring({ x: 10, y: 5, z: 10 });
+    const rotation = spring({ x: 0, y: 0, z: 0 });
+
+    const path = [
+        { x: 0, y: 0, z: 0, rotation: { x: 0, y: 0, z: 0 } },
+        { x: 2, y: 1, z: -1, rotation: { x: 0, y: Math.PI / 4, z: 0 } },
+        {
+            x: -1,
+            y: 2,
+            z: 1,
+            rotation: { x: Math.PI / 4, y: 0, z: Math.PI / 4 },
+        },
+        { x: 0, y: 0, z: 0, rotation: { x: 0, y: Math.PI * 2, z: 0 } },
+    ];
+
+    $: duckPosition = [progress * 10, progress * 5, 10];
+
+    $: if (index < path.length - 1) {
+        rotation.set(
+            interpolate(path[index].rotation, path[index + 1].rotation, offset),
+        );
+        position.set(interpolate(path[index], path[index + 1], offset));
+    }
 </script>
 
 <Container>
-    <Scroller bind:progress {top} {threshold} {bottom}>
+    <Scroller
+        bind:index
+        bind:count
+        bind:offset
+        bind:progress
+        {top}
+        {threshold}
+        {bottom}
+    >
         <div slot="background" class="h-[100vh]">
             <div class="w-full mx-auto h-full">
                 <Canvas>
@@ -31,14 +68,14 @@
                         makeDefault
                         position={[10, 10, 10]}
                         on:create={({ ref }) => {
-                            ref.lookAt(...duckPosition)
+                            ref.lookAt($position);
                         }}
                     >
                         <OrbitControls />
                     </T.PerspectiveCamera>
                     <T.Group
-                        rotation.y={rotation}
-                        position={duckPosition}
+                        position={$position}
+                        rotation={$rotation}
                         scale={[2, 2, 2]}
                     >
                         <Duck />
